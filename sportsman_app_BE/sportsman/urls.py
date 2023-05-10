7b51c06eacfc59b027374ea9feb1dc5bdc9055bc
Hamza Delic <delic7262@gmail.com>
2023-05-10 04:52:40 +0200

    Updated registration for users, made registration for owners and updated models User and Owner

--- sportsman_app_BE/sportsman/urls.py
+++ sportsman_app_BE/sportsman/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registration/',views.registration,name='registration'),
+    path('registration/user/',views.registrationUser,name='registrationUser'),
+    path('registration/owner/',views.registrationOwner,name='registrationOwner'),
     path('login/',views.login,name='login')
 ]
--- sportsman_app_BE/sportsman/views.py
+++ sportsman_app_BE/sportsman/views.py
@@ -9,7 +9,7 @@ from .models import *
 # Create your views here.
 
 @api_view(['POST'])
-def registration(request):
+def registrationUser(request):
     name = request.data.get('name')
     surname = request.data.get('surname')
     username = request.data.get('username')
@@ -17,22 +17,48 @@ def registration(request):
     phone = request.data.get('phone')
     password = request.data.get('password')
     repeatedPassword = request.data.get('repeatedPassword')
-    typeOfUser = request.data.get('typeOfUser')
     city = request.data.get('city')
     age = request.data.get('age')
     sports = request.data.get('interests')
-    if(sports == []):
+    if not sports:
         interests = ""
     else:
         interests = json.dumps({"interests": sports})
     user = User.objects.filter(email=email)
-    if (password != repeatedPassword):
+    if password != repeatedPassword:
         return JsonResponse({'status': False, 'message': "Lozinke se ne podudaraju"}, status=400)
     elif len(user) > 0:
         return JsonResponse({'status': False, 'message': "Email je već registrovan."}, status=400)
     else:
         User.objects.create(name=name, surname=surname, username=username, email=email,
-                            typeOfUser=typeOfUser, tel_number=phone, city=city, age=age, interests=interests,
+                            tel_number=phone, city=city, age=age, interests=interests,
+                            password=make_password(password))
+        return JsonResponse({'status': True, 'message': "Uspješno ste se registrovali."}, status=201)
+
+@api_view(['POST'])
+def registrationOwner(request):
+    name = request.data.get('name')
+    surname = request.data.get('surname')
+    username = request.data.get('username')
+    email = request.data.get('email')
+    phone = request.data.get('phone')
+    password = request.data.get('password')
+    repeatedPassword = request.data.get('repeatedPassword')
+    city = request.data.get('city')
+    capacity = request.data.get('capacity')
+    street = request.data.get('street')
+    streetNumber = request.data.get('streetNumber')
+    type = request.data.get('type')
+    location = json.dumps({"city": city, "street": street, "streetNumber": streetNumber})
+    owner = Owner.objects.filter(email=email)
+
+    if password != repeatedPassword:
+        return JsonResponse({'status': False, 'message': "Lozinke se ne podudaraju"}, status=400)
+    elif len(owner) > 0:
+        return JsonResponse({'status': False, 'message': "Email je već registrovan."}, status=400)
+    else:
+        Owner.objects.create(name=name, surname=surname, username=username, email=email,
+                            tel_number=phone, location=location, capacity=capacity, type=type,
                             password=make_password(password))
         return JsonResponse({'status': True, 'message': "Uspješno ste se registrovali."}, status=201)
 
