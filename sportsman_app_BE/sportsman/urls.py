b5ed2f01ac365b3723d904559961c4839a9de45e
Almir Becirovic <almircodeb@gmail.com>
2023-05-13 17:53:57 +0200

    Added method for getting specific user and method to update specific user

--- sportsman_app_BE/sportsman/urls.py
+++ sportsman_app_BE/sportsman/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('registration/player/',views.registrationPlayer,name='registrationPlayer'),
     path('registration/owner/',views.registrationOwner,name='registrationOwner'),
+    path('player/<int:id>/', views.get_user_data, name="getUserData"),
+    path('update/player/<int:id>/', views.update_user, name="updateUser"),
     path('login/',views.login,name='login')
 ]
